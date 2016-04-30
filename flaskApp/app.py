1b10046343d461922f248c06f18663dbb415c0a1
thechristian <christianotoo@gmail.com>
2016-04-30 13:14:11 +0000

    checking uploaded file size

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -3,8 +3,9 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024 #upload file size allowed 3MB
 
-ALLOWED_EXTENSIONS = set(['csv','txt '])
+ALLOWED_EXTENSIONS = set(['csv','txt '])    #file extensions allowed
 
 
 #checking for appropriate file extensions
@@ -17,15 +18,17 @@ def allowed_file(filename):
 def main():
     return render_template('index.html')	#web interface - form
 
-
 @app.route('/upload', methods=['GET', 'POST'])	#getting all methods from the form
 def upload_file():
     if request.method == 'POST': 	#checking if its a post method
         f = request.files['dataFile'] 	#get file name from web interface
         if f and allowed_file(f.filename):
-            filepath = os.path.join('uploaded', 'newfile')	# file saving destination and a file name
+            filepath = os.path.join('uploaded', 'uploadedNewfile')	# file saving destination and a file name
             f.save(filepath) # save file to destination
             checkForDuplicate(filepath)
+        else:
+            return "Error! Check file extension and upload again."
+    return render_template('done.html')
 
 if __name__ == "__main__":
     app.debug = True
