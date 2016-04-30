d57569121de7af148e7a7a0457a6c40b523b6465
thechristian <christianotoo@gmail.com>
2016-04-30 08:10:28 +0000

    commented some lines in app.py

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -7,17 +7,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('index.html')	#web interface - form
 
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['GET', 'POST'])	#getting all methods from the form
 def upload_file():
-    if request.method == 'POST':
-        f = request.files['dataFile']
-        filepath = os.path.join('uploaded', 'newfile')
-        f.save(filepath)
-        print type(f)
-        checkForDuplicate(filepath)
+    if request.method == 'POST': 	#checking if its a post method
+        f = request.files['dataFile'] 	#get file name from web interface
+        filepath = os.path.join('uploaded', 'newfile')	# file saving destination and a file name
+        f.save(filepath) # save file to destination
+        checkForDuplicate(filepath) 
 
 if __name__ == "__main__":
     app.debug = True
