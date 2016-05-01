1386de875e809738cdbf71ef57198165c604e567
thechristian <christianotoo@gmail.com>
2016-05-01 08:06:55 +0000

    1. added time, date and random number to the user uploaded file before saving. 2. added success.html to the template directory

--- flaskApp/app.py
+++ flaskApp/app.py
@@ -1,12 +1,21 @@
 from dupEntries import checkForDuplicate
 import os
 from flask import Flask, render_template, request, redirect, url_for
+import sys
+import random
+import datetime
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024 #upload file size allowed 3MB
 
 ALLOWED_EXTENSIONS = set(['csv','txt '])    #file extensions allowed
 
+for i in xrange(1):
+    timer = datetime.datetime.now()  # get date and time
+    timeAndDate = timer.isoformat()  # geting time and date without spaces
+    fullTime = timeAndDate.split(".")[0]  # removing unused string from the date and time
+    randomNumber = '%04.4f' % random.random()  # generate a random number to make the time unigue
+    finalTimeAndDate = '-'.join([fullTime, randomNumber])  # add time and date to the random number
 
 #checking for appropriate file extensions
 def allowed_file(filename):
@@ -23,12 +32,13 @@ def upload_file():
     if request.method == 'POST': 	#checking if its a post method
         f = request.files['dataFile'] 	#get file name from web interface
         if f and allowed_file(f.filename):
-            filepath = os.path.join('uploaded', 'uploadedNewfile')	# file saving destination and a file name
+            filepath = os.path.join('uploaded', 'userFile.csv-' + finalTimeAndDate)	# file saving destination and a file name with date
+            #os.rename('userFile.csv', 'userFile.csv-' + finalTimeAndDate)
             f.save(filepath) # save file to destination
             checkForDuplicate(filepath)
         else:
             return "Error! Check file extension and upload again."
-    return render_template('done.html')
+    return render_template('success.html')
 
 if __name__ == "__main__":
     app.debug = True
