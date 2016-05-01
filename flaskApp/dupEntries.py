e4098010271a7940c826340d082713dc8b0f2503
thechristian <christianotoo@gmail.com>
2016-05-01 08:40:21 +0000

    added a directory path for saving separated files

--- flaskApp/dupEntries.py
+++ flaskApp/dupEntries.py
@@ -16,12 +16,12 @@ def checkForDuplicate(location):
         for line in filename:
             if line not in newLines:
                 newLines.append(line)     # adding to the newLine list
-                noDuplicate = open('newFile.csv', 'w')  #creating a new file to save entries without duplicate
+                noDuplicate = open('newfiles/newFile.csv', 'w')  #creating a new file to save entries without duplicate
                 noDuplicate.write(''.join(newLines))  # writing to the new file
                 noDuplicate.close()		# closing the file after writing
             elif line in newLines:      #checking if each line entry already exist in the newline list
                 dupLines.append(line)	# adding to the dupLines list
-                duplicated = open('duplicate.csv', 'w')     #creating a new file to save duplicate entries
+                duplicated = open('duplicatefiles/duplicate.csv', 'w')     #creating a new file to save duplicate entries
                 duplicated.write(''.join(dupLines))   # writing to the new file
                 duplicated.close()  # closing the file after writing
     for i in xrange(1):
@@ -30,7 +30,7 @@ def checkForDuplicate(location):
         fullTime = timeAndDate.split(".")[0]	#removing unused string from the date and time
         randomNumber = '%04.4f' % random.random()	# generate a random number to make the time unigue
         finalTimeAndDate = '-'.join([fullTime, randomNumber]) 	#add time and date to the random number
-    os.rename('duplicate.csv', 'duplicate.csv-' + finalTimeAndDate) 	#rename the output file with finalTimeAndDate
-    os.rename('newFile.csv', 'newFile.csv-' + finalTimeAndDate)		#same
+    os.rename('duplicatefiles/duplicate.csv', 'duplicatefiles/duplicate.csv-' + finalTimeAndDate) 	#rename the output file with finalTimeAndDate
+    os.rename('newfiles/newFile.csv', 'newfiles/newFile.csv-' + finalTimeAndDate)		#same
    # print "All done -  Entries separated from duplicates and saved separately"
 
