89bb0842d9d4f937c7b2d23bd19fc02497dfd986
thechristian <christianotoo@gmail.com>
2016-07-05 23:16:39 +0000

    regex

--- emailval.py
+++ emailval.py
@@ -1,1 +1,8 @@
 import re
+
+invalidemail = []
+emailkey = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$'
+matchemail = re.search(emailkey, 'thechristian@versified.xyz.gh')
+validemail = matchemail.group()
+if matchemail:
+    print validemail
