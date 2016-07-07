5f814432e3d65ae7a049eca704ed9048f309b414
thechristian <christianotoo@gmail.com>
2016-07-07 00:31:44 +0000

    added few more lines to emailval.py

--- emailval.py
+++ emailval.py
@@ -1,8 +1,20 @@
 import re
 
-invalidemail = []
+# Pandas email column extraction code comes here. All emails
+# extracted is put into inputemails list.
+
+inputemails = ['thechristian@versified.xyz.gh', 'gdg']        # Emails from uploaded file. Email key would be used to test each entry.
+validemail = []         # will contain entries that pass as a valid email
+invalidemail = []       # will contain entries that does not pass as a valid email
 emailkey = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$'
-matchemail = re.search(emailkey, 'thechristian@versified.xyz.gh')
-validemail = matchemail.group()
-if matchemail:
-    print validemail
+
+def emailvalidator():
+    for email in inputemails:
+        checkemail = re.search(emailkey, email)
+        if checkemail:
+            matchemail = checkemail.group()
+            validemail.append(matchemail)
+        else:
+            invalidemail.append(email)
+
+emailvalidator()
