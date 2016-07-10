d688e1988dc8d8e9069574b05b2fd145f875e35a
thechristian <christianotoo@gmail.com>
2016-07-10 10:58:07 +0000

    used pandas to save dupEntries

--- dupEntries.py
+++ dupEntries.py
@@ -1,5 +1,5 @@
 import csv, glob
-import datetime
+import pandas as pd
 import os
 import sys
 import random
@@ -17,28 +17,15 @@ def checkForDuplicate(location):
             if line not in newLines:
                 newLines.append(line)     # adding to the newLine list
                 noDuplicate = open('newfiles/newFile.csv', 'w')  # creating a new file to save entries without duplicate
-                noDuplicate.write(''.join(newLines))  # writing to the new file
-                noDuplicate.close()		# closing the file after writing
+                dataframe = pd.DataFrame(newLines)
+                dataframe.to_csv(noDuplicate)
+                # noDuplicate.write(''.join(newLines))  # writing to the new file
+                # noDuplicate.close()		# closing the file after writing
             elif line in newLines:      # checking if each line entry already exist in the newline list
                 dupLines.append(line)	 # adding to the dupLines list
-                duplicated = open('static/duplicates/duplicate.csv', 'w')   # creating a new file to save duplicate entries
-                duplicated.write(''.join(dupLines))   # writing to the new file
-                duplicated.close()  # closing the file after writing
-    for i in xrange(1):
-        timer = datetime.datetime.now() 	# get date and time
-        timeAndDate = timer.isoformat()		# getting time and date without spaces
-        fullTime = timeAndDate.split(".")[0]    # removing unused string from the date and time
-        randomNumber = '%04.4f' % random.random()   # generate a random number to make the time unigue
-        finalTimeAndDate = '-'.join([fullTime, randomNumber]) 	# add time and date to the random number
-
-    # filepathdup = ('duplicatefiles/duplicate.csv')
-    # filepathnew = ('newfiles/newFile.csv')
-
-    # if os.path.exists(filepathdup):
-    #     os.rename(filepathdup, 'duplicatefiles/duplicate.csv-' + finalTimeAndDate)  # rename the output file with finalTimeAndDate
-    #     os.rename(filepathnew, 'newfiles/newFile.csv-' + finalTimeAndDate)  # same
-    # elif os.path.exists(filepathnew):
-    #     os.rename(filepathnew, 'newfiles/newFile.csv-' + finalTimeAndDate)  # rename the output file with finalTimeAndDate
-
-   # print "All done -  Entries separated from duplicates and saved separately"
+                duplicated = open('duplicatefiles/duplicate.csv', 'w')   # creating a new file to save duplicate entries
+                dataframe = pd.DataFrame(dupLines)
+                dataframe.to_csv(duplicated)
+                # duplicated.write(''.join(dupLines))   # writing to the new file
+                # duplicated.close()  # closing the file after writing
 
