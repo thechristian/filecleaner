b6a0ba7b199e124864abf59fdc541d0aabd2477e
thechristian <christianotoo@gmail.com>
2016-07-10 11:00:43 +0000

    add few code lines

--- emailval.py
+++ emailval.py
@@ -1,20 +1,33 @@
 import re
+import pandas as pd
 
-# Pandas email column extraction code comes here. All emails
-# extracted is put into inputemails list.
-
-inputemails = ['thechristian@versified.xyz.gh', 'gdg']        # Emails from uploaded file. Email key would be used to test each entry.
 validemail = []         # will contain entries that pass as a valid email
-invalidemail = []       # will contain entries that does not pass as a valid email
+invalidemailcol = []     # will contain entries that does not pass as a valid email
 emailkey = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$'
 
-def emailvalidator():
-    for email in inputemails:
+
+def emailvalidator(x, y):
+    i = 0
+    filename = str(x)
+    emailcolname = str(y)
+    data = pd.read_csv(filename, index_col=False)
+    emails = data.loc[:, emailcolname]
+    for email in emails:
         checkemail = re.search(emailkey, email)
         if checkemail:
             matchemail = checkemail.group()
             validemail.append(matchemail)
+            emailfile = open('emails/validemails.csv', 'w')  # create a file to contain valid emails
+            dataframe = pd.DataFrame(validemail, columns=["Valid Emails"])
+            dataframe.to_csv(emailfile, index=False)
+            # emailfile.write(''.join(validemail))  # save to the file
         else:
-            invalidemail.append(email)
+            i =i+ 1
+            index = str(i)
+            invalidemailcol.append(index)
+            invalidemailfile = open('emails/emailcolnum.csv', 'w')  # create a file to contain invalid emails col numbs
+            dataframe = pd.DataFrame(invalidemailcol, columns=["Invalid Email Column Numbers"])
+            dataframe.to_csv(invalidemailfile, index=False)
+            # invalidemailfile.write(''.join(invalidemailcol))
 
-emailvalidator()
+
