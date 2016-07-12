8a71e4284a0945d64b9694d6b788917f77b873a0
thechristian <christianotoo@gmail.com>
2016-07-12 22:29:33 +0000

    excel to csv file

--- convertocsv.py
+++ convertocsv.py
@@ -1,1 +1,16 @@
 import pandas as pd
+import xlrd
+import xlwt
+import csv
+from utils import get_random_id
+import os
+
+
+def excel2csv(exlfilename, exlsheetname):
+    workbook = xlrd.open_workbook(exlfilename)
+    worksheet = workbook.sheet_by_name(exlsheetname)
+    csvfile = os.path.join('uploads', 'frmxl2csv-' + get_random_id())
+    dataframe = pd.DataFrame(worksheet)
+    dataframe.to_csv(csvfile)
+
+    return csvfile
