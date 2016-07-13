568021c60867eb1f072bc6005117f57b99418656
thechristian <christianotoo@gmail.com>
2016-07-13 17:49:29 +0000

    removed unnecessary comments

--- dupEntries.py
+++ dupEntries.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 import sys
 import random
+from pandas import DataFrame
 
 newLines = list()		# list for holding new entries
 dupLines = list()		# list for holding duplicate entries
@@ -17,15 +18,11 @@ def checkForDuplicate(location):
             if line not in newLines:
                 newLines.append(line)     # adding to the newLine list
                 noDuplicate = open('newfiles/newFile.csv', 'w')  # creating a new file to save entries without duplicate
-                dataframe = pd.DataFrame(newLines)
-                dataframe.to_csv(noDuplicate)
-                # noDuplicate.write(''.join(newLines))  # writing to the new file
-                # noDuplicate.close()		# closing the file after writing
+                df = pd.DataFrame(newLines)
+                df.to_csv(noDuplicate)
+
             elif line in newLines:      # checking if each line entry already exist in the newline list
                 dupLines.append(line)	 # adding to the dupLines list
                 duplicated = open('duplicatefiles/duplicate.csv', 'w')   # creating a new file to save duplicate entries
-                dataframe = pd.DataFrame(dupLines)
-                dataframe.to_csv(duplicated)
-                # duplicated.write(''.join(dupLines))   # writing to the new file
-                # duplicated.close()  # closing the file after writing
-
+                df = pd.DataFrame(dupLines)
+                df.to_csv(duplicated)
