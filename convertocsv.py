de1099df485debaecc1bbf54cdd03ff06697b73a
thechristian <christianotoo@gmail.com>
2016-07-13 11:48:54 +0000

    removed unnecessary comments

--- convertocsv.py
+++ convertocsv.py
@@ -6,11 +6,15 @@ from utils import get_random_id
 import os
 
 
-def excel2csv(exlfilename, exlsheetname):
-    workbook = xlrd.open_workbook(exlfilename)
-    worksheet = workbook.sheet_by_name(exlsheetname)
-    csvfile = os.path.join('uploads', 'frmxl2csv-' + get_random_id())
-    dataframe = pd.DataFrame(worksheet)
-    dataframe.to_csv(csvfile)
-
-    return csvfile
+def excel2csv(x, y):
+    try:
+        exlfilename = str(x)
+        exlsheetname = str(y)
+        workbook = xlrd.open_workbook(exlfilename)
+        worksheet = workbook.sheet_by_name(exlsheetname)
+        csvfile = os.path.join('uploads', 'frmxl2csv-' + get_random_id())
+        dataframe = pd.DataFrame(worksheet)
+        dataframe.to_csv(csvfile)
+        return csvfile
+    except:
+        print "Unsupported file. upload an Excel File"
