eb0592a2b2323d9d277a269556ab2ea4e2e995d8
thechristian <christianotoo@gmail.com>
2016-07-14 17:05:15 +0000

    used pandas instead of raw python

--- dupEntries.py
+++ dupEntries.py
@@ -1,28 +1,14 @@
-import csv, glob
 import pandas as pd
-import os
-import sys
-import random
-from pandas import DataFrame
-
-newLines = list()		# list for holding new entries
-dupLines = list()		# list for holding duplicate entries
 
 
 def checkForDuplicate(location):
-    files = glob.glob(location)
-    for eachfile in files:
-        filename = open(eachfile, 'r')
+    datafile = pd.read_csv(location)
+    df = pd.DataFrame(datafile)
 
-        for line in filename:
-            if line not in newLines:
-                newLines.append(line)     # adding to the newLine list
-                noDuplicate = open('newfiles/newFile.csv', 'w')  # creating a new file to save entries without duplicate
-                df = pd.DataFrame(newLines)
-                df.to_csv(noDuplicate)
-
-            elif line in newLines:      # checking if each line entry already exist in the newline list
-                dupLines.append(line)	 # adding to the dupLines list
-                duplicated = open('duplicatefiles/duplicate.csv', 'w')   # creating a new file to save duplicate entries
-                df = pd.DataFrame(dupLines)
-                df.to_csv(duplicated)
+    df['Is_Duplicated'] = df.duplicated()
+    noduplicates = df.loc[df['Is_Duplicated'] == False]
+    duplicates = df.loc[df['Is_Duplicated'] == True]
+    del noduplicates['Is_Duplicated']
+    noduplicates.to_csv('noduplicates/noduplicates.csv', index=False, encoding='utf-8')
+    del duplicates['Is_Duplicated']
+    duplicates.to_csv('duplicates/duplicates.csv', index=False, encoding='utf-8')
