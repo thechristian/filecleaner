dfb9581ce7ff09a969615964d2cba52cac8a6766
thechristian <christianotoo@gmail.com>
2016-07-14 18:52:41 +0000

    save in .xlsx

--- dupEntries.py
+++ dupEntries.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import xlsxwriter
 
 
-def checkForDuplicate(location):
+def checkForDuplicate(location, sheetname):
+    str(sheetname)
     datafile = pd.read_csv(location)
     df = pd.DataFrame(datafile)
 
@@ -9,6 +11,12 @@ def checkForDuplicate(location):
     noduplicates = df.loc[df['Is_Duplicated'] == False]
     duplicates = df.loc[df['Is_Duplicated'] == True]
     del noduplicates['Is_Duplicated']
-    noduplicates.to_csv('noduplicates/noduplicates.csv', index=False, encoding='utf-8')
+    writer1 = pd.ExcelWriter('noduplicates/noduplicates.xlsx', engine='xlsxwriter')
+    noduplicates.to_excel(writer1, sheet_name=sheetname, index=False)
+    writer1.save()
+    # noduplicates.to_csv('noduplicates/noduplicates.csv', index=False, encoding='utf-8')
     del duplicates['Is_Duplicated']
-    duplicates.to_csv('duplicates/duplicates.csv', index=False, encoding='utf-8')
+    writer2 = pd.ExcelWriter('duplicates/duplicates.xlsx', engine='xlsxwriter')
+    duplicates.to_excel(writer1, sheet_name=sheetname, index=False)
+    writer2.save()
+    #duplicates.to_csv('duplicates/duplicates.csv', index=False, encoding='utf-8')
