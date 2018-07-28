2ad67b9f6082bc1f9cecad4268b0165b4719adfe
Ebo Turkson <mturkson23@gmail.com>
2018-07-28 12:07:04 +0000

    modify comparison algo

--- compareFiles.py
+++ compareFiles.py
@@ -1,3 +1,4 @@
+import os
 import hashlib
 from flask import Flask, render_template, request, redirect, url_for
 
@@ -6,7 +7,7 @@ messsage2 = hashlib.sha256()
 
 
 def checkFile(file1, file2, userfolder):
-    folder = os.path.join('email',userfolder,'')
+    folder = os.path.join(userfolder,'')
     openfile1 = open(file1, mode='r')  # opening file in read mode
     openfile2 = open(file2, mode='r')
     content1 = openfile1.read()  # reading the content of the file
@@ -15,12 +16,12 @@ def checkFile(file1, file2, userfolder):
     messsage2.update(content2)
     hashed1 = messsage1.hexdigest()  # generating the hash value of the file content
     hashed2 = messsage2.hexdigest()
-    hashfile1 = open('hashes/hashvalue1', 'w')  # creating a file to save the hash values
-    hashfile2 = open('hashes/hashvalue2', 'w')
-    hashfile1.write(hashed1)  # saving the hash values to the file
-    hashfile2.write(hashed2)
+    # hashfile1 = open('hashes/hashvalue1', 'w')  # creating a file to save the hash values
+    # hashfile2 = open('hashes/hashvalue2', 'w')
+    # hashfile1.write(hashed1)  # saving the hash values to the file
+    # hashfile2.write(hashed2)
     hashstring1 = str(hashed1)
     hashstring2 = str(hashed2)
-    return hashstring1, hashstring2
     # hashfile1.close()
     # hashfile2.close()
+    return hashstring1, hashstring2
